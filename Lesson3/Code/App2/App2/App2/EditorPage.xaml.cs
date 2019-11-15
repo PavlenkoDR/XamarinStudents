331f520b6ab96be13b4e965a9d1e62ef8d8fbac7
PavlenkoDR <pavlenkocraft@gmail.com>
2019-11-16 08:25:52 +1000

    Lesson3 Code

---
+++ Lesson3/Code/App2/App2/App2/EditorPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EditorPage : ContentPage
+    {
+        public string text { private set; get; }
+        public EditorPage()
+        {
+            InitializeComponent();
+            text = null;
+        }
+
+        public EditorPage(string x)
+        {
+            InitializeComponent();
+            TextEditor.Text = x;
+            text = x;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            text = TextEditor.Text;
+            Navigation.PopAsync() ;
+        }
+    }
+}
---
+++ Lesson4/Example/Example/Example/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Example
+{
+    public class SuperClass
+    {
+        public string FirstField { get; set; }
+        public string SecondField { get; set; }
+        public int FontSize { get; set; }
+        public ImageSource Image { get; set; }
+    }
+
+    public class ImageResource : IMarkupExtension
+    {
+        public string Source { get; set; }
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Source == null)
+            {
+                return null;
+            }
+            var imageSource = ImageSource.FromResource(Source);
+
+            return imageSource;
+        }
+    }
+    public class EntryValidation : TriggerAction<Entry>
+    {
+        protected override void Invoke(Entry sender)
+        {
+            if (!int.TryParse(sender.Text, out var number))
+            {
+                sender.BackgroundColor = Color.Red;
+            }
+            else
+            {
+                sender.BackgroundColor = Color.White;
+            }
+        }
+    }
+
+    public class CustomCell : ViewCell
+    {
+        Label Text, Description;
+        Image image;
+    }
+
+    [DesignTimeVisible(false)]
+    public partial class MainPage : CarouselPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
